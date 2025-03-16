517caae902e551c17283cc57f4b7d0088f6a70ce
mu <user>
2025-03-16 11:27:09 +0800

    update

--- script/scjkynet.script.js
+++ script/scjkynet.script.js
@@ -1,13 +1,10 @@
+// 获取所有课程列表
 (async function () {
-  (await fetch("/sd-api/event/resourcePageNew/selectTeachInfoByPage/0?catalogId=-1&gradeId=-1&labelId=-1" +
-          "&noteId=-1&pageNo=3&pageSize=500&queryType=0&resourceFamily=-1&resourceName=&resourceType=0" +
-          "&sortType=1&stageId=-1&subjectId=-1&versionId=-1",
-          {headers: {authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvcGVuQWNjb3VudElkIjoiNzE5NTM1MjMxMDEwNDc2MDMyIiwidGVuYW50SWQiOiIwIiwicGxhdGZvcm1UeXBlIjoicGMiLCJpZCI6IjcxOTUzNTIzMTA3MzM5MDU5MyIsIm9wZW5Vc2VySW5mb0lkIjoiNzE3NDMzMDE4OTE0MDUxMTA1IiwidXNlck5hbWUiOiIxNTUyMDcyMjA1MiIsImV4cCI6MTcyNTA0MDgwMCwib3JnSWQiOiI0NDYwMTIzMTI1MjA4ODg2MTAiLCJ1c2VySW5mb0lkIjoiNzE5NTM1MjMxMDczMzkwNTkyIiwiY3JlYXRlVGltZXN0YW1wIjoiMTcyMjUyMDA0NTM4MiJ9.cEEp90pSH2e47nQhYSfUjuGZYN9ZdpihmgaXZZo_7hI"}})
-  ).json().then(r => {
-  })
-
-
-  ;
+  let authorization = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvcGVuQWNjb3VudElkIjoiNzE5NTM1MjMxMDEwNDc2MDMyIiwidGVuYW50SWQiOiIwIiwicGxhdGZvcm1UeXBlIjoicGMiLCJpZCI6IjcxOTUzNTIzMTA3MzM5MDU5MyIsIm9wZW5Vc2VySW5mb0lkIjoiNzE3NDMzMDE4OTE0MDUxMTA1IiwidXNlck5hbWUiOiIxNTUyMDcyMjA1MiIsImV4cCI6MTczMzk0MDAwMCwib3JnSWQiOiI0NDYwMTIzMTI1MjA4ODg2MTAiLCJ1c2VySW5mb0lkIjoiNzE5NTM1MjMxMDczMzkwNTkyIiwiY3JlYXRlVGltZXN0YW1wIjoiMTczMTQxMzkyMDkzMyJ9.cNUDZhGcTTVXvP6ZWeGumy8Lf8OB7HWWHdcccXwR-FE";
+  let api = "/sd-api/event/resourcePageNew/selectTeachInfoByPage/0?catalogId=-1&gradeId=-1&labelId=-1&noteId=-1&queryType=0" +
+      "&resourceFamily=-1&resourceName=&resourceType=0&sortType=1&stageId=-1&subjectId=-1&versionId=-1";
+  return (await fetch(`${api}&pageSize=500&pageNo=3`, {headers: {authorization}})).json()
+      .then(r => r.data.data.resResourceList.map(e => ({id: e.id, name: e.name, crt: new Date(e.createTime).toISOString()})));
 })();
 
 // 继续教育审批学时
