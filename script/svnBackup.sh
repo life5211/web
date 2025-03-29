8d3c76ac90fcc4f7a57c8fa82fa3c2ef24c3d883
usr <eei81@qq.com>
2025-03-29 16:02:32 +0800

    svn script

--- script/svnBackup.sh
+++ script/svnBackup.sh
@@ -1,34 +1,32 @@
 #!/bin/bash
-folder=YunPan
-cd "${folder}/"
+pth=$(pwd)
 funLog() {
-  echo -e $(date "+[ %Y-%m-%d %H:%M:%S ] ") "  ==>>  " $* >>"../${folder}-run.log/"
+  echo -e $(date "+[ %Y-%m-%d %H:%M:%S ] ") "  ==>>  " $* >>"../${pth##*/}-script-run.log"
   if [[ $1 != 0 ]]; then
-    echo -e " [[ Error ]] $* " >>"../${folder}-Error.log/"error.log
+    echo -e " [[ Error ]] $* " >>"../${pth##*/}-script-error.log"
     exit 1
   fi
 }
 svn log
-funLog $? "运行服务器连接测试"
+funLog $? "Network Test"
 result=$(svn status)
 if [[ $result ]]; then
-  funLog 0 "[Info]\\n $result"
-  #  funLog 0 "Add force to svn"
+  funLog 0 "[File Update Info]\\n $result"
   svn add . --force
+  #  funLog 0 "SVN adds all modifications."
   delFiles=$(svn status | grep '^\!')
   if [[ $delFiles ]]; then
-    #  SVN删除本地缺失文件
+    #  Delete local missing files by SVN
     svn st | grep '^\!' | sed 's/^! */"/' | sed 's/$/"/g' | xargs svn del --force
     #    svn st | grep '^\!' | tr '^\!' ' ' | sed 's/^ *//' | sed 's/[ ]/\\ /g' | sed 's/[\]/\//g' | xargs svn del --force
     #    svn st | grep '^\!' | sed 's/^! *//' | sed 's/[ ]/\\ /g' | sed 's/[\]/\//g' | xargs svn del --force
     #    svn st | grep '^\!' | awk '{print $2}' | sed 's/[\]/\//g' | sed 's/[ ]/\\ /g' | xargs svn del --force
   fi
-  #同步到本地并且提交到仓库
   svn update
-  svn commit . -m "脚本自动同步"
-  funLog $? "\n脚本自动提交到版本库"
-#  funLog $? "\n $(svn commit . -m "脚本自动同步") \n脚本自动提交到版本库"
+  svn commit . -m "Automatically upload to svn through script."
+  funLog $? "Automatically upload to svn through script."
+#  funLog $? "\n $(svn commit . -m "Automatically upload to svn through script.") Automatically upload to svn through script."
 else
   svn update
-  funLog $? "本地没有更新,同步vs数据"
+  funLog $? "No update locally, synchronizing data from VS."
 fi
