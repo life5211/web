f2be445f095f152d06f02256476e5e13f2b3b5f5
win7 <->
2025-04-03 15:24:35 +0800

    update

--- auto/play.zgzjzj.user.js
+++ auto/play.zgzjzj.user.js
@@ -18,40 +18,44 @@ function logUtil(msg, f) {
   if (f) alert(msg);
 }
 document.userI = setInterval((_) => {
-  let video = document.querySelector("video");
+  let video = $("video")[0];
   if (!video) return logUtil("没有video媒体");
   video.muted = true;
-  if (document.querySelector("div.feedBackBox")) {
-    document.querySelector("div.feedBackBox h3 img").click();
-    return logUtil("关闭弹窗");
+  if ($("div.feedBackBox").length) {
+    $("div.feedBackBox h3 img")[0].click();
+    logUtil("关闭弹窗");
   }
-  if (!video.paused) {
-    return logUtil("正在播放，继续等待，否者判断暂停原因");
+  if (
+    $("div.el-message-box__wrapper i.el-message-box__close.el-icon-close")
+      .length
+  ) {
+    $(
+      "div.el-message-box__wrapper i.el-message-box__close.el-icon-close"
+    )[0].click();
+    logUtil("关闭弹窗");
   }
+  // let minute = $("#app div.course-progress a");
   let progress = document.querySelector(
-    "#app div.main-box div.course-progress > div > div > span:nth-child(3)"
+    "div.course-progress span:nth-child(3)"
   )?.innerText;
-  if (progress && video.ended) {
-    logUtil("当前视频播放完成");
-    if (progress !== "100%") {
-      document
-        .querySelector(
-          "#app div.main-box div.navigate > ul > li.nextdontcheatorshit"
-        )
-        .click();
-      logUtil("播放下一章节视频");
-    } else {
-      let next =
-        document.querySelector(
-          "#app div.course-catlog-new > div.course-swipper div.swiper-slide.swiper-slide-next img"
-        ) ||
-        document.querySelector(
-          "#app div.course-catlog-new > div.course-swipper div.swiper-slide.swiper-slide-prev img"
-        );
-      if (!next) return logUtil("当前课程包学习完成！", true);
-      next.click();
-      logUtil("播放下一课程视频");
-    }
+  logUtil(`课程播放进度：${progress}`);
+  if (progress !== "100%" && video.ended) {
+    logUtil("当前章节视频播放完成,播放下一章节视频");
+    document.querySelector("#app ul > li.nextdontcheatorshit").click();
+  }
+  if (progress === "100%") {
+    logUtil("当前课程学习完成");
+    $("div.topnav>li:last-child")[0].click();
+    let next =
+      $("#app div.course-swipper div.swiper-slide-next img")[0] ||
+      $("#app div.course-swipper div.swiper-slide-prev img")[0];
+    if (!next) return logUtil("恭喜你，当前课程包全部学习完成！", true);
+    next.click();
+    logUtil("播放下一课程视频");
+  }
+
+  if (!video.paused) {
+    return logUtil("正在播放……");
   }
   if (video.paused) video.play().then(logUtil).catch(logUtil);
 }, 66666);
