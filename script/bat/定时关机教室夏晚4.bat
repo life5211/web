dc751573a10f888cb4fc52f5d755ece9e3a2bbec
win7 <->
2025-05-28 20:55:34 +0800

    schtasks shutdown

--- script/bat/定时关机教室夏晚4.bat
+++ script/bat/定时关机教室夏晚4.bat
@@ -4,7 +4,8 @@ schtasks /delete /tn "daily1705shutdown" /f
 schtasks /delete /tn "daily1745shutdown" /f
 schtasks /delete /tn "daily2025shutdown" /f
 schtasks /delete /tn "daily2105shutdown" /f
+schtasks /delete /tn "daily2141shutdown" /f
 
 schtasks /create /tn "daily1215shutdown" /tr "shutdown /s /t 300" /sc daily /st 12:10:00 
 schtasks /create /tn "daily1745shutdown" /tr "shutdown /s /t 300" /sc daily /st 17:40:00 
-schtasks /create /tn "daily2105shutdown" /tr "shutdown /s /t 300" /sc daily /st 21:00:00 
+schtasks /create /tn "daily2141shutdown" /tr "shutdown /s /t 60" /sc daily /st 21:40:00 
