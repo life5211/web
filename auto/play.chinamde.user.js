48146aeb364fcfd0b9d501604ad61bac7d45d5f0
usr <eei81@qq.com>
2025-07-17 18:42:23 +0800

    fix 成都理工大学继续教育

--- auto/play.chinamde.user.js
+++ auto/play.chinamde.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         成都理工大学继续教育自动播放
+// @name         成都理工大学继续教育快速播放
 // @namespace    http://tampermonkey.net/
 // @version      0.17
 // @description  成都理工大学多课程自动连续播放
 // @icon         https://enclosure-file.oss-cn-zhangjiakou.aliyuncs.com/2115192812559196224/20240624/2024%E6%96%B0%E7%89%88%E6%88%90%E9%83%BD%E7%90%86%E5%B7%A5.png
-// @match        https://sczj.chinamde.cn/play/*
+// @match        https://sczj.chinamde.cn/play*
 // @downloadURL  https://life5211.github.io/web/auto/play.chinamde.user.js
 // @updateURL    https://life5211.github.io/web/auto/play.chinamde.user.js
 // @noframes
@@ -17,7 +17,7 @@ window.addEventListener("load", function play() {
       $localSet = (k, v) => localStorage.setItem(k, JSON.stringify(v)),
       $log = (msg, f) => {
         console.log(msg);
-        let log = $localGet("log", "[]");
+        let log = $localGet("log", []);
         log.push(`[${new Date().toLocaleString()}]${msg}`);
         $localSet("log", log);
         if (f) alert(msg);
@@ -27,7 +27,7 @@ window.addEventListener("load", function play() {
     let video = $q("video");
     if (!video) return $log("没有video媒体");
     video.muted = true;
-    video.playbackRate = 16;
+    if (video.playbackRate < 16) video.playbackRate = 16;
 
     $qa("img[alt=展开]").forEach(e => e.click());
     let curr = $q("img[src='/imgs/a.gif']")?.parentElement;
