52cd45b85360e616ec55bc2e2931ebe0485fc325
mu <user>
2025-09-30 11:34:36 +0800

    电子教材下载fix

--- auto/smart.zxx.textbook.user.js
+++ auto/smart.zxx.textbook.user.js
@@ -7,26 +7,15 @@
 // @icon         https://basic.smartedu.cn/img/entrance.ebbe1413.png
 // @match        https://basic.smartedu.cn/tchMaterial/detail*
 // @license      MIT
+// @noframes
+// @grant        unsafeWindow
+// @grant        GM_registerMenuCommand
+// @grant        GM_unregisterMenuCommand
 // ==/UserScript==
 
-// 强制显示所有工具栏按钮JS注入CSS
 (function () {
-  const style = document.createElement('style');
-  style.textContent = `
-  .toolbar, .findbar, .download, .print {
-    display: block !important;
-    visibility: visible !important;
-  }
-`;
-  document.head.appendChild(style);
-})();
-// 添加下载事件
-window.addEventListener("load", function () {
-  let div = document.createElement('div');
-  div.innerHTML = '<span id="down" title="点击下载电子课本" style="color: cornflowerblue;cursor: pointer">下载电子课本</span>'
-  document.querySelector("div.theme-zxx-logo").parentNode.parentNode.append(div)
-  // document.querySelector("span.fish-breadcrumb-link").parentNode.append(div)
-  document.querySelector("#down").onclick = function downloadTextBook() {
+  if ("/tchMaterial/detail" === location.pathname)   GM_registerMenuCommand("下载电子课本", downloadTextBook);
+  function downloadTextBook() {
     let viewerSrc = document.getElementById("pdfPlayerFirefox").src;
 // 'https://basic.smartedu.cn/pdfjs/2.15/web/viewer.html?file=https://r1-ndr-private.ykt.cbern.com.cn/edu_product/esp/assets/ed5f6a59-0cc5-47e9-adc3-0033711700ea.pkg/义务教育教科书 物理 九年级 全一册_1725097556591.pdf&headers={"X-ND-AUTH":"MAC id=\\"7F938B205F876FC3A30551F3A4931383650FE2F51028AC11A7012EAFEE73D81CF1D19271D0CB74A75EC73EB8CC0A31C9FD26A853F92D8427\\",nonce=\\"1751897112002:EU6LTY64\\",mac=\\"RYS/ZUwKi3D4e9gbN8LfskUwADAnuNm7uR7llkKnsVo=\\""}';
     let pdfHref = viewerSrc.match(/https.+file=(https[^&]+)&headers=([^#]+)(#.*)?$/);
@@ -44,12 +33,5 @@ window.addEventListener("load", function () {
           window.URL.revokeObjectURL(url);
         })
         .catch(error => console.error('Download failed:', error));
-  };
-});
-(function () {
-  const authKey = Object.keys(localStorage).find(key => key.startsWith("ND_UC_AUTH"));
-  if (!authKey) return console.error("未找到 Access Token，请确保已登录！");
-  const tokenData = JSON.parse(localStorage.getItem(authKey));
-  const accessToken = JSON.parse(tokenData.value).access_token;
-  console.log("%cAccess Token:", "color: green; font-weight: bold", accessToken);
+  }
 })();
