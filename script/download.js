fedf1b8d52d31ee29109ac216b8e167ba8cff7f8
win11 <@>
2025-11-29 12:10:02 +0800

    Fix downloadXLSX

--- script/download.js
+++ script/download.js
@@ -1,10 +1,9 @@
-javascript:(async function downloadExcel(fileName, objArr) {
+// javascript:
+(async function downloadExcelByXLSX(fileName, objArr) {
   if (!objArr?.length) return alert("导出数据为空！");
-  (await fetch("https://cdn.sheetjs.com/xlsx-0.20.3/package/dist/xlsx.full.min.js"))
-      .text().then(eval).then(js => {
-    let workbook = XLSX.utils.book_new();
-    let worksheet = XLSX.utils.json_to_sheet(objArr);
-    XLSX.utils.book_append_sheet(workbook, worksheet, fileName);
-    XLSX.writeFile(workbook, `${fileName}.xlsx`);
-  })
-})(`Result${new Date().toLocaleString().replaceAll(/[^\d]/g, '')}`, temp2);
+  if (!window.XLSX) await (await fetch("https://cdn.sheetjs.com/xlsx-0.20.3/package/dist/xlsx.full.min.js")).text().then(eval).catch(console.log);
+  let workbook = XLSX.utils.book_new();
+  let worksheet = XLSX.utils.json_to_sheet(objArr);
+  XLSX.utils.book_append_sheet(workbook, worksheet, fileName);
+  XLSX.writeFile(workbook, `${fileName}.xlsx`);
+})(`Result${new Date().toLocaleString().replaceAll(/[^\d]/g, '')}`, window.temp6 || window.temp5 || window.temp4 || window.temp3 || window.temp2 || window.temp1);
