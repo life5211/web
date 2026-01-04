ffa5c085533065807103ed4568613dbd7cc9f8fd
mu <user>
2026-01-04 17:29:15 +0800

    feature 后台自动解析导入信息

--- auto/zk.ncjypt.user.js
+++ auto/zk.ncjypt.user.js
@@ -10,33 +10,27 @@
 // @match        *ncjypt.com/*
 // @match        https://zk.ncedu.net.cn/*
 // @match        *zk.ncedu.net.cn/*
-// @require      https://cdn.sheetjs.com/xlsx-0.20.2/package/dist/xlsx.full.min.js
-// @grant        GM_addStyle
-// @grant        unsafeWindow
-// @grant        GM_setValue
-// @grant        GM_getValue
-// @grant        GM_log
-// @grant        GM_listValues
-// @grant        GM_deleteValue
 // ==/UserScript==
 
-let studentsGrades, titles, studentsArr, gradesObj, next;
+(function () {
+  let studentsGrades, titles, studentsArr, gradesObj, next;
 
-function getGrades() {
-  studentsArr = JSON.parse(localStorage.getItem("students_info") || "[]");
-  gradesObj = JSON.parse(localStorage.getItem("grades_info") || "{}");
-  studentsGrades = studentsArr.map(stu => Object.assign({}, stu, gradesObj[stu.IdNo]));
-  titles = ["Name", "IdNo", "ExamNo", ...new Set(Object.values(gradesObj).flatMap(grade => Object.keys(grade)))];
-  next = studentsArr.filter(s => s?.IdNo && !gradesObj[s.IdNo]).pop();
-  dataShow();
-  console.log([studentsGrades, titles, studentsArr, gradesObj, next]);
-  return [studentsGrades, titles, studentsArr, gradesObj];
-}
+  function getGrades() {
+    studentsArr = JSON.parse(localStorage.getItem("students_info") || "[]");
+    gradesObj = JSON.parse(localStorage.getItem("grades_info") || "{}");
+    studentsGrades = studentsArr.map(stu => Object.assign({}, stu, gradesObj[stu.IdNo]));
+    titles = ["Name", "IdNo", "ExamNo", ...new Set(Object.values(gradesObj).flatMap(grade => Object.keys(grade)))];
+    next = studentsArr.filter(s => s?.IdNo && !gradesObj[s.IdNo]).pop();
+    dataShow();
+    console.log([studentsGrades, titles, studentsArr, gradesObj, next]);
+    return [studentsGrades, titles, studentsArr, gradesObj];
+  }
 
-function collect() {
-  if (!sessionStorage.getItem("collect_state")) return;
-  if (document.querySelector("#showInfo>table")?.innerText?.includes("还未开通成绩查询")) return;
-  if (location.pathname.startsWith("/nczk/zk/queryscoreby2img.asp")) {
+  function collect() {
+    if (!sessionStorage.getItem("collect_state")) return;
+    if (!location.pathname.startsWith("/nczk/zk/queryscoreby2img.asp")) return;
+    if (document.querySelector("#showInfo>table")?.innerText?.includes("还未开通成绩查询")) return;
+
     let name = document.querySelector("tr.tr-02>.tdvalue")?.innerText;
     // if (!name) return alert("数据查询失败，请重试！");
     // let idNumber = nameIds.substr(-19, 18);
@@ -51,21 +45,15 @@ function collect() {
     gradesObj[stuObj.IdNo] = stuObj;
     localStorage.setItem("grades_info", JSON.stringify(gradesObj));
     getGrades();
+    // 下一个考生成绩
+    if (!studentsArr?.length) return alert("请导入考生名单");
+    if (!next) return alert("采集完成");
+    setTimeout(_ => location.href = `/nczk/zk/queryscoreby2img.asp?t=${next.ExamNo},${encodeURI(next.Name)},${next.IdNo}`, Math.random() * 1500 + 600);
   }
-  // 下一个考生成绩
-  if (!studentsArr?.length) return alert("请导入考生名单");
-  if (!next) return alert("采集完成");
-  setTimeout(_ => location.href = `${location.origin}/nczk/zk/queryscoreby2img.asp?t=${next.ExamNo},${encodeURI(next.Name)},${next.IdNo}`, Math.random() * 1500 + 600);
-}
 
-function nextGrades() {
-
-}
-
-// (await fetch("https://cdn.sheetjs.com/xlsx-0.20.2/package/dist/xlsx.full.min.js")).text().then(eval);
-
-const div = document.createElement("div");
-div.innerHTML = `<div>
+  if (window.top === window) {
+    const div = document.createElement("div");
+    div.innerHTML = `<div>
     <textarea id="stuInfos" rows="2" cols="30" placeholder="学生信息表.csv中内容复制粘贴后点击导入"></textarea>
     <button onclick="importStuInfo()">信息导入</button> 
     <button onclick="downloadExportCsv()">查询结果导出</button>
@@ -74,73 +62,80 @@ div.innerHTML = `<div>
     <div id="result" style="max-height: 300px; overflow: auto"></div>
     <hr/>
 </div>`;
-document.body.insertBefore(div, document.body.firstChild);
-getGrades();
-collect();
+    document.body.insertBefore(div, document.body.firstChild);
+  }
+  getGrades();
+  collect();
 
-function getVal(obj, k, f) {
-  if (obj[k] instanceof Date) return obj[k].toLocaleDateString();
-  // if (f && /\d{8,}/.test(obj[k])) return `${obj[k]}`;
-  if (k in obj) return obj[k];
-  return "";
-}
+  function getVal(obj, k, f) {
+    if (obj[k] instanceof Date) return obj[k].toLocaleDateString();
+    // if (f && /\d{8,}/.test(obj[k])) return `${obj[k]}`;
+    if (k in obj) return obj[k];
+    return "";
+  }
 
-function dataShow() {
-  document.querySelector("button#coll").innerText = sessionStorage.getItem("collect_state") ? `暂停采集` : '开始采集';
-  if (!studentsGrades?.length) return;
-
-  document.querySelector('div#result').innerHTML = `<span>查询结果：${Object.values(gradesObj)?.length}</span>/<span>${studentsArr?.length || 0}</span>
+  function dataShow() {
+    document.querySelector("button#coll").innerText = sessionStorage.getItem("collect_state") ? `暂停采集` : '开始采集';
+    if (!studentsGrades?.length) return;
+    document.querySelector('div#result').innerHTML = `<span>查询结果：${Object.values(gradesObj)?.length}</span>/<span>${studentsArr?.length || 0}</span>
         <table border="1" style="border-collapse: collapse;border: 2px solid rgb(140 140 140);">
             <thead><tr>${titles.map(k => '<th>' + k + '</th>')}</tr></thead>
             <tbody>
             ${studentsGrades.map(stu => '<tr>' + titles.map(k => '<td>' + getVal(stu, k) + '</td>') + '</tr>').join("")}
             </tbody>
         </table>`;
-}
-
-
-document.collectionStateChange = function () {
-  if (sessionStorage.getItem("collect_state")) {
-    sessionStorage.removeItem("collect_state")
-  } else {
-    sessionStorage.setItem("collect_state", "run");
-    collect();
   }
-  dataShow();
-}
-
-document.importStuInfo = async function () {
-  const infoTxt = document.getElementById("stuInfos").value;
-  if (!infoTxt?.trim()) return alert("信息為空信息为空");
-  let stu = infoTxt.trim().split("\n")
-      .map((s) => s.split(/\s/))
-      .filter((s) => (s.length > 3) && s[5]?.length === 18)
-      .map(s => ({Name: s[6], ExamNo: s[24], IdNo: s[5]}));
-  localStorage.setItem("students_info", JSON.stringify(stu));
-  getGrades();
-}
-
-document.downloadExportCsv = function () {
-  // csv 导出
-  if (!studentsGrades.length) return alert("无成绩数据");
-  let student_string = studentsGrades.map(stu => titles.map(title => `${getVal(stu, title, true)}`).join("\t")).join("\r\n");
-  (function downloadCsv(fileName, content) {
-    let blob = new Blob([`\ufeff${content}`], {type: "text/csv;charset=utf-8"});
-    let link = document.createElement('a');
-    link.download = `${fileName}details_${new Date().toLocaleString()}.csv`;
-    link.href = URL.createObjectURL(blob);
-    link.click();
-  })("学生中考成绩单", `${titles.join("\t")}\r\n${student_string}`);
-}
 
 
-(function () {
+  document.collectionStateChange = function () {
+    if (sessionStorage.getItem("collect_state")) {
+      sessionStorage.removeItem("collect_state")
+    } else {
+      sessionStorage.setItem("collect_state", "run");
+      collect();
+    }
+    dataShow();
+  }
+
+  document.importStuInfo = function () {
+    importParse(document.getElementById("stuInfos").value);
+  }
+
+  function importParse(infoTxt) {
+    if (!infoTxt?.trim()) return alert("信息為空信息为空");
+    let stu = infoTxt.trim().split("\n")
+        .map((s) => s.split(/\s/))
+        .filter((s) => (s.length > 3) && s[5]?.length === 18)
+        .map(s => ({Name: s[6], ExamNo: s[24], IdNo: s[5]}));
+    localStorage.setItem("students_info", JSON.stringify(stu));
+    getGrades();
+  }
+
+  document.downloadExportCsv = function () {
+    // csv 导出
+    if (!studentsGrades.length) return alert("无成绩数据");
+    let student_string = studentsGrades.map(stu => titles.map(title => `${getVal(stu, title, true)}`).join("\t")).join("\r\n");
+    (function downloadCsv(fileName, content) {
+      let blob = new Blob([`\ufeff${content}`], {type: "text/csv;charset=utf-8"});
+      let link = document.createElement('a');
+      link.download = `${fileName}details_${new Date().toLocaleString()}.csv`;
+      link.href = URL.createObjectURL(blob);
+      link.click();
+    })("学生中考成绩单", `${titles.join("\t")}\r\n${student_string}`);
+  }
+
+
   // 学生信息采集
   if (location.pathname !== '/nczk/admin/querystudent.asp') return;
-  document.querySelector("input.butquery.command").addEventListener("click", async function (params) {
+  let span = document.createElement("span");
+  span.innerHTML = `<input type="button" value="自动导入基础信息" class="butquery command" id="autoSync">`;
+  document.querySelector("input.butquery.command").after(span);
+  span.addEventListener("click", async function (params) {
     let sid = document.getElementById("selectschoolid").value;
     let tid = document.getElementById("selecttestid").value;
+    if (!tid) return alert("请选择考试名称");
     let grade = document.getElementById("schoolgrade").value;
+    if (!grade) return alert("请选择年级");
     let rsp = await fetch(`/nczk/admin/asp/querystudentinfo_e.asp?tid=${tid}&grade=${grade}&cid=&sid=${sid}&class=&key=&page=1&apply=0&poor=0&timestamp=${Date.now()}`);
     if (!rsp.ok) return console.log(rsp.statusText);
     let text = await rsp.text();    // f0,temp/a8abb4bb284b5b27aa7cb790dc20f80b_学生信息表.csv
@@ -151,5 +146,6 @@ document.downloadExportCsv = function () {
     let response = await fetch(`/nczk/admin/${encodeURI(strings[1])}?timestamp=${Date.now()}`);
     const decoder = new TextDecoder('gbk');
     localStorage.csv = decoder.decode(await response.arrayBuffer());
+    importParse(localStorage.csv);
   });
 })();
